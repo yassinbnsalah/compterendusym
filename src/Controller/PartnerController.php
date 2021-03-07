851298cf89c3054f7790de0a9c849110208e707c
iheb karoui <karouiiheb4@gmail.com>
2021-03-07 21:41:20 +0100

    first commit

---
+++ src/Controller/PartnerController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PartnerController extends AbstractController
+{
+    public function index(): Response
+    {
+        return $this->render('partner/index.html.twig', [
+            'controller_name' => 'PartnerController',
+        ]);
+    }
+}
